faa11ca086aa2be313f4b26b19d232d5e9c85c12
alexandre <alexandre@pandascore.co>
2020-01-17 14:48:45 +0100

    f

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name="TaskBank",
     version="0.1.12",
     author="Alexandre Kempf",
-    author_email="alexanre.kempf@cri-paris.org",
+    author_email="alexandre.kempf@cri-paris.org",
     description="A library of machine learning tasks for HackDuck",
     long_description=long_description,
     package_dir={"": "src"},
